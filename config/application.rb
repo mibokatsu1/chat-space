c3409254d93dc77367cb242c0e8665141c841cfe
mibokatsu1 <mibo.katsu@gmail.com>
2020-06-08 22:03:44 +0900

    Change flash-massage's language to Japanese

--- config/application.rb
+++ config/application.rb
@@ -2,13 +2,10 @@ require_relative 'boot'
 
 require 'rails/all'
 
-# Require the gems listed in Gemfile, including any gems
-# you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
 module ChatSpace
   class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
     config.generators do |g|
       g.stylesheets false
@@ -17,9 +14,6 @@ module ChatSpace
       g.test_framework false
     end
 
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration can go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded after loading
-    # the framework and any gems in your application.
+    config.i18n.default_locale = :ja
   end
 end
